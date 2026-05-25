73c03e103425e2a949c414244dc65a441bf67a34
Smorden <mingjin.deng@gmail.com>
2026-05-25 16:57:19 +0800

    不要套装，利润用前置表

--- 临时需求/库存周转天数明细.sql
+++ 临时需求/库存周转天数明细.sql
@@ -32,7 +32,7 @@ with dim_sku as (
     )
 , profit_product_cost as (
     select dt, sku, sum(-product_cost_cny_fi) as profit_product_cost
-    from dws.dws_alct_theory_profit_sum_di
+    from dws.dws_alct_theory_profit_sum_order_di
     where dt between date_sub(curdate(), interval 60 day) and date_sub(curdate(), interval 1 day)
     group by dt, sku
     )
